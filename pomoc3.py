89215d1c46c92c16544ebdbd41aa0a267c4e7cea
Jan Matecha <jan.matecha@gmail.com>
2018-02-22 20:55:26 +0100

    pokracovani

---
+++ pomoc3.py
@@ -0,0 +1,16 @@
+#import d1motor
+#from machine import I2C, Pin
+#i2c = I2C(-1, Pin(5), Pin(4), freq=100000)
+#m1 = d1motor.Motor(0, i2c)
+#m1.speed(5000)
+
+#print("Zacatek main.py")
+
+
+#import test01
+
+#import blik
+
+#import motor
+
+#print("KONEC main.py")
